7a8a13ab46c61eb50b4efb11e8c737893fc1c611
jawadSaleem <jawadsaleem932@gmail.com>
2023-08-30 23:48:36 +0500

    designed hero section and footer along with other updates

---
+++ core.js
@@ -0,0 +1,8 @@
+// footer year js
+const footerYearArea = document.getElementById('footerDate');
+const currentYear = new Date().getFullYear();
+footerYearArea.innerHTML = currentYear;
+// console.log(currentYear);
+const getMyEmailAddress = () =>{
+    alert("You can mail me at : \n 'jawadsaleem932@gmail.com' or 'bukc688@gmail.com' . Thank you for reaching out to me.");
+}
