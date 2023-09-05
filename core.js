12c262bbd2c12fde87270329f512ed760383a7f7
jawadSaleem <jawadsaleem932@gmail.com>
2023-09-05 16:55:43 +0500

    Just completed all my design work

--- core.js
+++ core.js
@@ -6,3 +6,21 @@ footerYearArea.innerHTML = currentYear;
 const getMyEmailAddress = () =>{
     alert("You can mail me at : \n 'jawadsaleem932@gmail.com' or 'bukc688@gmail.com' . Thank you for reaching out to me.");
 }
+// form data retrieval and emtying inputs
+// const submitBtn = document.querySelector('#submissionBtn');
+// const inputs = document.querySelectorAll('#myInput');    
+// submitBtn.addEventListener('click',()=>{
+//     inputs.forEach((singleInput)=>{
+//         singleInput.value = "";
+//     });
+//     alert('Your response has been successfully recieved !');
+// });
+
+const inputs = document.querySelectorAll('#myInput');    
+const fetchData = (e) =>{
+    Array.from(inputs).forEach((singleInput)=>{
+        singleInput.value = '';
+    });
+    alert('Your response has been successfully recieved !');
+    return false;
+}
